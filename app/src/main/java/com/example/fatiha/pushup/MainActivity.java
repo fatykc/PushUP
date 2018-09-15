9face26de496b55edbf354bd9a2186aedd7b64a3
faty <fatihakaci@hotmail.com>
2018-09-15 15:32:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fatiha/pushup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.fatiha.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+
+
+    public void calcul(){
+        int i= 1;
+
+
+    }
+}
